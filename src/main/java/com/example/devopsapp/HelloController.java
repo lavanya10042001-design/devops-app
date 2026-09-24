7b48ecc21d474b44e9d8e3940da2566d2b04c2f3
Lavanya <lavanya10042001@gmail.com>
2026-09-24 19:07:26 +0530

    Test atomatic jenkins build

--- src/main/java/com/example/devopsapp/HelloController.java
+++ src/main/java/com/example/devopsapp/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Lavanya welcome to the DevOps learning!";
+        return "Hello Lavanya welcome to the DevOps learning journey!";
     }
 }
